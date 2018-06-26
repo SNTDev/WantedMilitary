099e02cf4f8b362464df16ffda884eb430b7bda3
SNTDev <lmj960417@gmail.com>
2018-06-26 23:28:39 +0900

    react 적용

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import LazyLoad from 'react-lazy-load';
+import { Card, Button, CardTitle, CardText, Row, Col, CardDeck } from 'reactstrap';
 import wantedMilitaryList from './WantedList.json';
 
 class App extends Component {
-  getWantedList() {
-    console.log(wantedMilitaryList);
+  getWantedCardWithIndex(index) {
+    return (
+      <Card body style={{margin: "20px"}}>
+            <CardTitle>{wantedMilitaryList[index].company_name}</CardTitle>
+            <CardText>{wantedMilitaryList[index].position}</CardText>
+            <Button>Go somewhere</Button>
+    </Card>
+    );
   }
 
   render() {
-    this.getWantedList();
+    let rows = [];
 
-    return (
-      <div>
-        {wantedMilitaryList.map((wantedData) => {
-          return (<Card body>
-            <CardTitle>{wantedData.company_name}</CardTitle>
-            <CardText>{wantedData.position}</CardText>
-            <Button>Go somewhere</Button>
-          </Card>);
-        })}
-      </div>
-    );
+    for (let i = 0; i < wantedMilitaryList.length; i += 3) {
+      rows.push(
+        <CardDeck>
+          {this.getWantedCardWithIndex(i)}
+          {i + 1 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i+1) : <Card/>}
+          {i + 2 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i+2) : <Card/>}
+        </CardDeck>
+      );
+    }
+
+    console.log(rows);
+    return rows;
   }
 }
 
