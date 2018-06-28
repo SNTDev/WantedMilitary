654c7c9edd5b9382b18cddb4eb0bc1c097373c76
SNTDev <lmj960417@gmail.com>
2018-06-28 22:50:54 +0900

    ul li  변경

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ import wantedMilitaryList from './WantedList.json';
 class App extends Component {
   getWantedCardWithIndex(index) {
     return (
+      <a target='_blank' href={'https://www.wanted.co.kr/wd/' + wantedMilitaryList[index].recruit_id} data-toggle="tooltip" data-placement="bottom" title={wantedMilitaryList[index].position}>
       <Card body style={{ margin: "20px" }}>
           <CardImg top height="200px" src={wantedMilitaryList[index].title_img} />
-          <CardBody>
-            <CardTitle>{wantedMilitaryList[index].company_name}</CardTitle>
-            <CardText style={{ fontWeight: "300", paddingLeft: "5%" }}>{wantedMilitaryList[index].position}</CardText>
-          </CardBody>
-          <a href={'https://www.wanted.co.kr/wd/' + wantedMilitaryList[index].recruit_id} class="effect-underline">지원하러 가기</a>
+          <CardTitle>{wantedMilitaryList[index].company_name}</CardTitle>
+          <CardText style={{ fontWeight: "300", paddingLeft: "5%" }}>{wantedMilitaryList[index].position}</CardText>
       </Card>
+      </a>
     );
   }
 
@@ -27,18 +26,23 @@ class App extends Component {
       return a.company_name > b.company_name ? 1 : a.company_name === b.company_name ? 0 : -1;
     });
 
-    for (let i = 0; i < wantedMilitaryList.length; i += 4) {
+    for (let i = 0; i < wantedMilitaryList.length; i ++) {
       rows.push(
-        <CardDeck>
+        <li>
           {this.getWantedCardWithIndex(i)}
-          {i + 1 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i + 1) : <Card />}
-          {i + 2 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i + 2) : <Card />}
-          {i + 3 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i + 3) : <Card />}
-        </CardDeck>
+        </li>
       );
+
+      /*{i + 1 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i + 1) : <Card />}
+          {i + 2 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i + 2) : <Card />}
+          {i + 3 < wantedMilitaryList.length ? this.getWantedCardWithIndex(i + 3) : <Card />}*/
     }
 
-    return rows;
+    return (
+      <ul>
+      {rows};
+      </ul>
+    );
   }
 }
 
